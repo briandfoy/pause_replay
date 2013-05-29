c031f4834740adb080f118daafb1f0681b41d967
brian d foy <brian.d.foy@gmail.com>
2013-05-29 10:50:45 -0500

    Add PAUSE emails

---
+++ get_modules.pl
@@ -0,0 +1,51 @@
+#!/Users/brian/bin/perls/perl5.14.2
+use v5.10;
+use utf8;
+use strict;
+use warnings;
+
+use File::Spec::Functions;
+use Net::NNTP;
+
+my $group = 'perl.modules';
+
+my $nntp = Net::NNTP->new('nntp.perl.org');
+my( $count, $first, $last ) = $nntp->group( $group );
+say "$count, $first, $last";
+
+my $dir = $group;
+mkdir $dir, 0755 unless -d $dir;
+
+foreach my $i ( $first .. $last ) {
+	my $file = catfile( $dir, "$i.txt" );
+	next if -e $file;
+	open my $fh, '>', $file;
+	my $article = $nntp->article( $i, $fh );
+	close $fh;
+	printf "%s: %d bytes\n", $file, -s $file;
+	sleep 1;
+	}
+
+$nntp->quit;
+           
+=head1 NAME
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 AVAILABILITY
+
+=head1 AUTHOR
+
+brian d foy C<< <brian.d.foy@gmail.com> >>
+
+=head1 COPYRIGHT
+
+Copyright 2012-2013, brian d foy C<< <brian.d.foy@gmail.com> >>
+
+=cut
+
+
+
+
